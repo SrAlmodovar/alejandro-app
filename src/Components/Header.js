17caa7023d2baff12b73c7734809eaf4196bae59
SrAlmodovar <bmthalejandro@gmail.com>
2024-10-24 13:35:54 +0200

    He modificado el archivo index.css para agregar reglas generales a la pagina y modificado el archivo app.css para agregar las reglas a la barra de navegacion

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,10 +4,12 @@ import logo from "../Icons_assets/Logo.svg";
 function Header(){
   return (
     <header>
+      <nav className="navbar">
       <div className="logo">
-        <img src={logo} alt="Little Lemon logo"></img>
+        <a href="#home">
+          <img className="logoImg" src={logo} alt="Little Lemon logo"></img>
+        </a>
       </div>
-      <nav>
         <ul>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
