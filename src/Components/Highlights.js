869418e15f73f842507219d9795a35a262946b1a
SrAlmodovar <bmthalejandro@gmail.com>
2024-10-29 18:31:19 +0100

    He creado las diferentes boxes de Highlights asi como preparado de forma responsive, he agregado las imagenes

--- src/Components/Highlights.js
+++ src/Components/Highlights.js
@@ -1,10 +1,28 @@
 import React from "react";
+import LemonDessert from "../Icons_assets/lemon dessert.jpg";
+import Bruchetta from "../Icons_assets/bruchetta.svg";
+import GreekSalad from "../Icons_assets/greek salad.jpg";
 
 function Highlights(){
   return (
-    <article>
-      <div className="contenedorPrincipal">
-        <p>as</p>
+    <article className="contenedorPrincipal">
+      <div className="highBox1">
+        <h2>Specials</h2>
+        <button>Online Menu</button>
+      </div>
+      <div className="highBox2">
+        <div className="highBoxDish">
+        <img src={Bruchetta} alt="Bruchetta"></img>
+        <p>s</p>
+        </div>
+        <div className="highBoxDish">
+        <img src={GreekSalad} alt="Greek salad"></img>
+          <p>s</p>
+        </div>
+        <div className="highBoxDish">
+        <img src={LemonDessert} alt="Lemon dessert"></img>
+        <p>s</p>
+        </div>
       </div>
     </article>
   );
